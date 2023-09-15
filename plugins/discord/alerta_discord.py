0aab0ee46b9005c4f78b573b9d4bdecd474a87b6
Alwin Antreich <alwin.antreich@croit.io>
2023-09-15 12:49:17 +0200

    plugin: add multi-channel support to discord
    The plugin can now send differenet environements to different channels.
    
    Signed-off-by: Alwin Antreich <alwin.antreich@croit.io>

--- plugins/discord/alerta_discord.py
+++ plugins/discord/alerta_discord.py
@@ -10,11 +10,16 @@ from alerta.plugins import PluginBase
 
 LOG = logging.getLogger('alerta.plugins.discord')
 
-DISCORD_WEBHOOK_URL = os.environ.get('DISCORD_WEBHOOK_URL') or app.config['DISCORD_WEBHOOK_URL']
-DISCORD_CONTENT     = os.environ.get('DISCORD_CONTENT') or app.config.get('DISCORD_CONTENT', '')
-DISCORD_ICON        = os.environ.get('DISCORD_ICON') or app.config.get('DISCORD_ICON', 'https://raw.githubusercontent.com/alerta/alerta-webui/master/public/favicon-128.png')
-DASHBOARD_URL       = os.environ.get('DASHBOARD_URL') or app.config['DASHBOARD_URL']
-ALERTA_USERNAME     = os.environ.get('ALERTA_USERNAME') or app.config.get('ALERTA_USERNAME', 'alerta')
+DISCORD_WEBHOOK_URL = os.environ.get(
+    'DISCORD_WEBHOOK_URL') or app.config.get('DISCORD_WEBHOOK_URL')
+DISCORD_CONTENT = os.environ.get(
+    'DISCORD_CONTENT') or app.config.get('DISCORD_CONTENT', '')
+DISCORD_ICON = os.environ.get('DISCORD_ICON') or app.config.get(
+    'DISCORD_ICON',
+    'https://raw.githubusercontent.com/alerta/alerta-webui/master/public/favicon-128.png')
+DASHBOARD_URL = os.environ.get('DASHBOARD_URL') or app.config['DASHBOARD_URL']
+ALERTA_USERNAME = os.environ.get(
+    'ALERTA_USERNAME') or app.config.get('ALERTA_USERNAME', 'alerta')
 
 # discord wants decimals instead of hex
 DEFAULT_SEVERITY_MAP = {
@@ -23,12 +28,12 @@ DEFAULT_SEVERITY_MAP = {
     'major': '16753920',  # orange
     'minor': '16776960',  # yellow
     'warning': '16753920',  # orange
-    'informational': '8421504',  #gray
+    'informational': '8421504',  # gray
     'debug': '8421504',  # gray
     'trace': '8421504',  # gray
     'cleared': '52224',  # green
     'ok': '52224'  # green
-    }
+}
 
 
 class PostMessage(PluginBase):
@@ -39,71 +44,83 @@ class PostMessage(PluginBase):
     def post_receive(self, alert):
         if alert.repeat:
             return
-        self._post_message(self._prepare_payload(alert))
+
+        url = self._get_environment(alert)
+        self._post_message(self._prepare_payload(alert), url)
 
     def status_change(self, alert, status, text):
-#        if status not in ['ack', 'assign']:
-#            return
-        self._post_message(self._prepare_payload(alert, status, text))
+        #        if status not in ['ack', 'assign']:
+        #            return
+        url = self._get_environment(alert)
+        self._post_message(self._prepare_payload(alert, status, text), url)
+
+    @staticmethod
+    def _get_environment(alert):
+        url = DISCORD_WEBHOOK_URL
+        if isinstance(DISCORD_WEBHOOK_URL, dict):
+            url = DISCORD_WEBHOOK_URL.get(alert.environment)
+
+        return url
 
     @staticmethod
     def _prepare_payload(alert, status=None, text=None):
         LOG.debug('Discord: %s, %s, %s', alert, status, text)
 
-        color = DEFAULT_SEVERITY_MAP.get(alert.severity) or DEFAULT_SEVERITY_MAP.get('informational')
+        color = DEFAULT_SEVERITY_MAP.get(
+            alert.severity) or DEFAULT_SEVERITY_MAP.get('informational')
 
         title = '[{event}]'.format(event=alert.event)
 
         status = (status if status else alert.status).capitalize()
 
         return {
-                "avatar_url": DISCORD_ICON,
-                "content": DISCORD_CONTENT,
-                "username": ALERTA_USERNAME,
-                "embeds": [{
-                    "url": DASHBOARD_URL,
-                    "title": title,
-                    "description": text,
-                    "color": color,
-                    "fields": [
-                        {
-                            "name": "Status",
-                            "value": status,
-                            "inline": True
-                        },
-                        {
-                            "name": "Serverity",
-                            "value": alert.severity.capitalize() or '',
-                            "inline": True
-                        },
-                        {
-                            "name": "Value",
-                            "value": alert.value or '',
-                        },
-                        {
-                            "name": "Resource",
-                            "value": alert.resource or '',
-                        },
-                        {
-                            "name": "Environment",
-                            "value": alert.environment or '',
-                            "inline": True
-                        },
-                        {
-                            "name": "Customer",
-                            "value": alert.customer or 'None',
-                            "inline": True
-                        }
-                    ],
-                    }],
-                }
+            "avatar_url": DISCORD_ICON,
+            "content": DISCORD_CONTENT,
+            "username": ALERTA_USERNAME,
+            "embeds": [{
+                "url": DASHBOARD_URL,
+                "title": title,
+                "description": text,
+                "color": color,
+                "fields": [
+                    {
+                        "name": "Status",
+                        "value": status,
+                        "inline": True
+                    },
+                    {
+                        "name": "Serverity",
+                        "value": alert.severity.capitalize() or '',
+                        "inline": True
+                    },
+                    {
+                        "name": "Value",
+                        "value": alert.value or '',
+                    },
+                    {
+                        "name": "Resource",
+                        "value": alert.resource or '',
+                    },
+                    {
+                        "name": "Environment",
+                        "value": alert.environment or '',
+                        "inline": True
+                    },
+                    {
+                        "name": "Customer",
+                        "value": alert.customer or 'None',
+                        "inline": True
+                    }
+                ],
+            }],
+        }
 
     @staticmethod
-    def _post_message(payload):
+    def _post_message(payload, url):
         LOG.debug('Discord: %s', payload)
 
         try:
-            r = requests.post(DISCORD_WEBHOOK_URL, json=payload, timeout=2)
+            r = requests.post(url, json=payload, timeout=2)
         except Exception as e:
             raise RuntimeError("Discord: ERROR - %s" % e)
 
